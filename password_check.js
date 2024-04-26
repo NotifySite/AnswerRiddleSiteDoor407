8ed08b31e29c53b7e3fff539e1f4dca7145e4634
bookWorm21 <ilyagolosovoice@gmail.com>
2024-04-26 21:27:23 +0300

    add crypting and show result

--- password_check.js
+++ password_check.js
@@ -1,27 +1,26 @@
+const contentBox = document.querySelector('.content');
+const input = document.getElementById('password-input');
+
 function checkPassword() {
-    const input = document.getElementById('password-input');
-    allert(ipnut.value)
-    const inputHash = sha256(input.value); // Хэшируем введенное значение с использованием SHA-256
-
-    // Хэш правильного пароля (замените на свой)
-    const correctPasswordHash = '4c7e6c8d5f2a5c1c6d2f3f7e9e8d8c1a0b6d4c9a7b1f0b3a5c2d2f1a8c6e9'; // Пример: 'password'
-
+    const inputHash = sha256(input.value);
+    const correctPasswordHash = '6468e22921802c6d2264cb4c452ef810f2915330f7ef71d5a3d4b5c5bddd4f52';
     if (inputHash === correctPasswordHash) {
-        alert('Пароль верный!');
+        const result = CryptoJS.AES.decrypt('U2FsdGVkX1/DWgFkxJyFvBxV5JlOBuTcKN5ZQiL7fOY=', input.value).toString(CryptoJS.enc.Utf8);
+        input.style.display = 'none';
+        contentBox.innerHTML = `<div class="correct-password-block">${result}</div>`;
     } else {
-        input.style.borderColor = 'red'; // Подсвечиваем поле ввода красным
-        alert('Неверный пароль!');
+        input.style.borderColor = 'red';
     }
 }
 
-document.getElementById('password-input').addEventListener('keypress', function(event) {
+document.getElementById('password-input').addEventListener('keypress', function (event) {
     if (event.key === 'Enter') {
         checkPassword();
     }
 });
 
-document.getElementById('password-input').addEventListener('input', function() {
+document.getElementById('password-input').addEventListener('input', function () {
     if (this.value.length > 10) {
-        this.value = this.value.slice(0, 10); // Обрезаем введенное значение до 10 символов
+        this.value = this.value.slice(0, 10);
     }
 });

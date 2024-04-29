9687894b69f906a77dd68fe0320766461ce76369
bookWorm21 <ilyagolosovoice@gmail.com>
2024-04-29 03:31:27 +0300

    add ok button

--- password_check.js
+++ password_check.js
@@ -1,4 +1,5 @@
 const contentBox = document.querySelector('.content');
+const form = document.getElementById('password-form');
 const input = document.getElementById('password-input');
 
 function checkPassword() {
@@ -6,21 +7,20 @@ function checkPassword() {
     const correctPasswordHash = '6468e22921802c6d2264cb4c452ef810f2915330f7ef71d5a3d4b5c5bddd4f52';
     if (inputHash === correctPasswordHash) {
         const result = CryptoJS.AES.decrypt('U2FsdGVkX1/DWgFkxJyFvBxV5JlOBuTcKN5ZQiL7fOY=', input.value).toString(CryptoJS.enc.Utf8);
-        input.style.display = 'none';
+        form.style.display = 'none';
         contentBox.innerHTML = `<div class="correct-password-block">${result}</div>`;
     } else {
         input.style.borderColor = 'red';
     }
 }
 
-document.getElementById('password-input').addEventListener('keydown', function (event) {
-    if (event.key === 'Enter') {
-        checkPassword();
-    }
-});
-
-document.getElementById('password-input').addEventListener('input', function () {
+input.addEventListener('input', function () {
     if (this.value.length > 10) {
         this.value = this.value.slice(0, 10);
     }
+});
+
+form.addEventListener('submit', function (event) {
+    event.preventDefault();
+    checkPassword();
 });

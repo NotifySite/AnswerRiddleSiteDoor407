8cabe5b4fd797c58d7b84421ef08b33f06d2c528
bookWorm21 <ilyagolosovoice@gmail.com>
2024-05-20 22:32:04 +0300

    change hash

--- password_check.js
+++ password_check.js
@@ -4,7 +4,7 @@ const input = document.getElementById('password-input');
 
 function checkPassword() {
     const inputHash = sha256(input.value);
-    const correctPasswordHash = 'ZsNCkyqnwY978fohKqARBp/C5Q4Mfp5Mc/yqaFGFTEc=';
+    const correctPasswordHash = '3f4OjUYq9mH4HbNlicOYgtwPIzB4W12AzTTy9SCtYY8=';
     if (inputHash === correctPasswordHash) {
         const result = CryptoJS.AES.decrypt('U2FsdGVkX19hT7jsiRO7jSx9aPe3PtCsTl+qwH6p9aM=', input.value).toString(CryptoJS.enc.Utf8);
         form.style.display = 'none';
